8b9c41dae5910016bde150d88b7b089faa6df789
Sean Lavine <sean@digit.co>
2019-04-08 16:06:05 -0700

    use mongoose v4 behavior for `required` on Array

--- index.js
+++ index.js
@@ -7,5 +7,11 @@
 'use strict';
 
 const mongoose = require('./lib/');
+
+// https://mongoosejs.com/docs/migrating_to_5.html#id-getter
 mongoose.set('objectIdGetter', false);
+
+// https://mongoosejs.com/docs/migrating_to_5.html#array-required
+mongoose.Schema.Types.Array.checkRequired(v => Array.isArray(v) && v.length);
+
 module.exports = mongoose;

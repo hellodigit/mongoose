621c59559a1f4bb7d606e92db23d8181162a8578
Sean Lavine <sean@digit.co>
2019-11-14 11:30:40 -0800

    use legacy/deprecated find and modify

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const mongoose = require('./lib/');
 // https://mongoosejs.com/docs/migrating_to_5.html#id-getter
 mongoose.set('objectIdGetter', false);
 
-// https://mongoosejs.com/docs/deprecations.html#-findandmodify-
-mongoose.set('useFindAndModify', false);
-
 // https://mongoosejs.com/docs/migrating_to_5.html#array-required
 mongoose.Schema.Types.Array.checkRequired(v => Array.isArray(v) && v.length);
 
